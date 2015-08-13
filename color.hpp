c35e3a5d44f2d3373690899d420b7cec93edb782
Phil Cooper-King <phil@cooperking.net>
2015-08-13 09:19:12 +0100

    Added basic color funcs.

--- color.hpp
+++ color.hpp
@@ -47,10 +47,131 @@ enum class swizzle {
   second_to_last,     // eg: ARGB -> ABGR
 };
 
-inline color        color_swizzle(const color swizzle_color, const swizzle swizzle_format);
+inline color        color_swizzle(const color swizzle_color, const swizzle swizzle_format); // not impl
 
 
 // ** IMPL ** //
+
+namespace detail
+{
+  inline uint8_t to_uint8(const float c)
+  {
+    return static_cast<uint8_t>((c / 1.f) * 255);
+  }
+
+  inline float to_float(const uint8_t c)
+  {
+    return static_cast<float>(c) / 255.f;
+  }
+}
+
+
+
+color
+color_init(const uint32_t hex_components)
+{
+  color = hex_components;
+}
+
+
+color
+color_init(const uint8_t c1, const uint8_t c2, const uint8_t c3, const uint8_t c4)
+{
+  color combined_channels = (uint32_t)((c1 << 24) | (c2 << 16) | (c3 << 8) | (c4 << 0));
+  return combined_channels;
+}
+
+
+color
+color_init(const float c1, const float c2, const float c3, const float c4)
+{
+  return color_init(to_uint8(c1), to_uint8(c2), to_uint8(c3), to_uint8(c4));
+}
+
+
+color
+color_init(const std::array<float, 4> &channels)
+{
+  return color_init(channels.at(0), channels.at(1), channels.at(2), channels.at(3));
+}
+
+
+color
+color_init(const std::array<uint8_t, 4> &channels)
+{
+  return color_init(channels.at(0), channels.at(1), channels.at(2), channels.at(3));
+}
+
+
+color
+color_init(const float channels[], const uint32_t size)
+{
+  return color_init(channels[0], channels[1], channels[2], channels[4]);
+}
+
+
+color
+color_init(const uint8_t channels[] const uint32_t size)
+{
+  return color_init(channels[0], channels[1], channels[2], channels[4]);
+}
+
+
+uint8_t
+color_get_channel_1i(const color get_color_channel)
+{
+  return static_cast<uint8_t>((get_color_channel >> 24) & 0xFF);
+}
+
+
+uint8_t
+color_get_channel_2i(const color get_color_channel)
+{
+  return static_cast<uint8_t>((get_color_channel >> 16) & 0xFF);
+}
+
+
+uint8_t
+color_get_channel_3i(const color get_color_channel)
+{
+  return static_cast<uint8_t>((get_color_channel >> 8) & 0xFF);
+}
+
+
+uint8_t
+color_get_channel_4i(const color get_color_channel)
+{
+  return static_cast<uint8_t>((get_color_channel >> 0) & 0xFF);
+}
+
+
+float
+color_get_channel_1f(const color get_color_channel)
+{
+  return to_float(color_get_channel_1i(get_color_channel));
+}
+
+
+float
+color_get_channel_2f(const color get_color_channel)
+{
+  return to_float(color_get_channel_2i(get_color_channel));
+}
+
+
+float
+color_get_channel_3f(const color get_color_channel)
+{
+  return to_float(color_get_channel_3i(get_color_channel));
+}
+
+
+float
+color_get_channel_4f(const color get_color_channel)
+{
+  return to_float(color_get_channel_4i(get_color_channel));
+}
+
 
 
 } // namespace
