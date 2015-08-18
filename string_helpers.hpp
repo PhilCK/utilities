0f066efa46c5ce5e1de552b0f90f300acb3748e5
Phil Cooper-King <phil@cooperking.net>
2015-08-18 19:41:33 +0100

    String helper

--- string_helpers.hpp
+++ string_helpers.hpp
@@ -2,17 +2,21 @@
 #define STRING_HELPERS_INCLUDED_CC0C05E9_7D4D_4A69_B002_6E59BDC031B9
 
 
-namepsace util {
+#include <string>
 
 
-std::string get_contents_from_file(const std::string &filename)
+namespace util {
+
+
+std::string
+get_contents_from_file(const std::string &filename)
 {
   // Open the file in binary mode
   FILE *f = fopen(filename.c_str(), "rb");
   
   // Determine the size of the file
   fseek(f, 0, SEEK_END);
-  int len = ftell(f);
+  long len = ftell(f);
   fseek(f, 0, SEEK_SET);
   
   // Load the entire file in one call
